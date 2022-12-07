fd00bd11c95ec5f6d9b68637b5ddfb774d09ce59
Your Name <you@example.com>
2022-12-07 23:33:26 +0500

    new

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('about/', AboutView.as_view()),
+    path('author/', AuthorView.as_view()),
+    path('contact/', ContactView.as_view()),
+    path('full-left/', FullleftView.as_view()),
+    path('full-right/', FullrightView.as_view()),
+    path('full/', FullView.as_view()),
+    path('list-right/', ListrightView.as_view()),
+    path('list-left/', ListleftView.as_view()),
+    path('list', ListView.as_view()),
+    path('post-details-1/', Postd1View.as_view()),
+    path('post-details-2/', Postd2View.as_view()),
+    path('post-elements/', PostelementsView.as_view()),
+    path('privacy-policy/', PrivacyView.as_view()),
+    path('terms-conditions/', TermsView.as_view()),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post, TopPost, LatestPost
+from django.views.generic import TemplateView
 
 def home(request):
     posts = Post.objects.all()
@@ -11,3 +12,46 @@ def home(request):
         "lposts": lposts
     }
     return render(request, "index.html", context)
+
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+class AuthorView(TemplateView):
+    template_name = "author.html"
+
+class ContactView(TemplateView):
+    template_name = "contact.html"
+
+class FullleftView(TemplateView):
+    template_name = "index-full-left.html"
+
+class FullrightView(TemplateView):
+    template_name = "index-full-rigth.html"
+
+class FullView(TemplateView):
+    template_name = "index-full.html"
+
+class ListrightView(TemplateView):
+    template_name = "index-list-right.html"
+
+class ListleftView(TemplateView):
+    template_name = "index-list-left.html"
+
+class ListView(TemplateView):
+    template_name = "index-list.html"
+
+class Postd1View(TemplateView):
+    template_name = "post-details-1.html"
+
+class Postd2View(TemplateView):
+    template_name = "post-details-2.html"
+
+class PostelementsView(TemplateView):
+    template_name = "post-elemants.html"
+
+class PrivacyView(TemplateView):
+    template_name = "privacy-policy.html"
+
+class TermsView(TemplateView):
+    template_name = "terms-conditions.html"
